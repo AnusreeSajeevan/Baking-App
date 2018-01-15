181dc76b8126982724afc16145cd28d4bc12ddf1
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-15 11:40:44 +0530

    Add butterknife dependency

--- app/src/main/java/com/example/anu/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/anu/bakingapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.anu.bakingapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.anu.bakingapp.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
