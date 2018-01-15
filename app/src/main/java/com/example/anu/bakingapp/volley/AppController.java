1290a3a3b34af94c40cf9364c9fc496a8b06d217
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-15 11:52:22 +0530

    Remove unused import

--- app/src/main/java/com/example/anu/bakingapp/volley/AppController.java
+++ app/src/main/java/com/example/anu/bakingapp/volley/AppController.java
@@ -12,7 +12,6 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.Volley;
-import com.facebook.stetho.Stetho;
 
 import java.io.ByteArrayOutputStream;
 
