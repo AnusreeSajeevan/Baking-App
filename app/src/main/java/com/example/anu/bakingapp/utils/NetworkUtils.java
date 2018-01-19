dc38fbf172e1666dd6be43369f43e720a6383bc2
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2018-01-19 12:28:51 +0530

    Add butterknife, gson and room gradle dependemcies

--- app/src/main/java/com/example/anu/bakingapp/utils/NetworkUtils.java
+++ app/src/main/java/com/example/anu/bakingapp/utils/NetworkUtils.java
@@ -4,5 +4,4 @@ class NetworkUtils {
 
     //the url where the recipe listing json is located
     private static final String RECIPE_LISTING_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-
 }
